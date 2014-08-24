d620a67d4d91587b120224d41709651d8f5ad65d
Ken Ohata <ohata@daraf.co.jp>
2014-08-25 01:32:44 +0900

    add a column original_task_id

--- db/migrate/20140824111659_create_tasks.rb
+++ db/migrate/20140824111659_create_tasks.rb
@@ -3,11 +3,13 @@ class CreateTasks < ActiveRecord::Migration
     create_table :tasks do |t|
       t.string :name
       t.integer :weight
+      t.integer :original_task_id
       t.string :logging_type
 
       t.timestamps
 
       t.index :name
+      t.index :original_task_id
     end
   end
 end
