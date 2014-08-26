54d0a801c8dc4b3e62cc27cdf2f1c6f49f1f362b
Ken Ohata <ohata@daraf.co.jp>
2014-08-26 23:35:05 +0900

    add columns super_task_id and is_super_task

--- db/migrate/20140824111659_create_tasks.rb
+++ db/migrate/20140824111659_create_tasks.rb
@@ -5,15 +5,19 @@ class CreateTasks < ActiveRecord::Migration
       t.integer :weight
       t.integer :original_task_id
       t.integer :previous_task_id
+      t.integer :super_task_id
       t.string :logging_type
       t.string :task_state
+      t.boolean :is_super_task
 
       t.timestamps
 
       t.index :name
       t.index :original_task_id
       t.index :previous_task_id
+      t.index :super_task_id
       t.index :task_state
+      t.index :is_super_task
     end
   end
 end
